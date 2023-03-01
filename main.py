dfb981a014086645422ac56a0260858696e13cec
Cooper <cjo113@uit.no>
2023-03-01 15:17:40 +0100

    created class structure

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+# import pygame
+
+class Game:
+    pass
+
+class GameObject:
+    pass
+
+class Obstacle(GameObject):
+    pass
+
+class Character(GameObject):
+    pass
+
+class Boid(Character):
+    pass
+
+class Hoik(Character):
+    pass
