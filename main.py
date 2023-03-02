9b76bb94aaf4acfc772691a2944a7675d3b4ba42
Cooper <cjo113@uit.no>
2023-03-02 14:41:27 +0100

    boids steer toward avg position of others nearby

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ import random
 
 class Game:
     BKG_COLOR = (61, 61, 61)
-    BOID_COLOR = (90, 142, 191)
+    BOID_COLOR = (109, 157, 201)
 
     def __init__(self, size: tuple[int, int]):
         pygame.init()
         self.surface = pygame.display.set_mode(size)
         pygame.display.set_caption("Boids")
 
-        self._set_up_objects(5)
+        self._set_up_objects(16)
 
     def _set_up_objects(self, num_boids):
-        self.objects = [
+        self.boids = [
             self._generate_boid()
             for _ in range(num_boids)
         ]
+
+        self.objects = self.boids
 
     def _generate_boid(self):
         # Initial position is randomized within the game window
@@ -27,7 +29,7 @@ class Game:
         pos = pygame.Vector2(x, y)
 
         # Initial velocity has given speed in a random direction
-        speed = 5
+        speed = 3
         vel = pygame.Vector2(speed, 0).rotate(random.randint(0, 359))
 
         boid = Boid(self, pos, vel, self.BOID_COLOR)
@@ -74,39 +76,54 @@ class Obstacle(GameObject):
     pass
 
 class Boid(GameObject):
-    def _get_flock(self, r):
+    def _get_local_flock(self, r: int):
         # Returns `Flock` object with other boids within radius r
-        pass
+        local_flock = Flock([
+            boid
+            for boid in self.game.boids
+            if (boid.pos - self.pos).magnitude() <= r
+        ])
+        return local_flock
+    
+    def _toward_local_flock_center(self) -> pygame.Vector2:
+        weight = 0.01 # scale factor for resulting change in velocity
 
-    def _stay_within_bounds(self):
+        local_flock = self._get_local_flock(r=60)
+        dv = weight*(local_flock.avg_pos() - self.pos)
+
+        return dv
+
+    def _stay_within_bounds(self) -> pygame.Vector2:
         buffer = 30
         x_min, y_min = buffer, buffer
         x_max = self.game.surface.get_width() - buffer
         y_max = self.game.surface.get_height() - buffer
 
-        vel = pygame.Vector2(0, 0)
-
-        rebound_speed = 3
+        rebound_speed = 2
+        dv = pygame.Vector2(0, 0)
 
         if self.pos.x < x_min:
-            vel.x = rebound_speed
+            dv.x = rebound_speed
         elif self.pos.x > x_max:
-            vel.x = -rebound_speed
+            dv.x = -rebound_speed
 
         if self.pos.y < y_min:
-            vel.y = rebound_speed
+            dv.y = rebound_speed
         elif self.pos.y > y_max:
-            vel.y = -rebound_speed
+            dv.y = -rebound_speed
 
-        return vel
+        return dv
 
-    def _addtl_vel(self) -> pygame.Vector2:
-        v2 = self._stay_within_bounds()
+    def _accl(self) -> pygame.Vector2:
+        # Calculate 'acceleration' i.e. change in velocity
+        dv1 = self._toward_local_flock_center()
+        dv2 = self._stay_within_bounds()
 
-        return v2
+        dv = dv1 + dv2
+        return dv
 
     def move(self):
-        self.vel += self._addtl_vel()
+        self.vel += self._accl()
         self.dir = self.vel.normalize()
 
         self.pos.x += self.vel.x
@@ -132,9 +149,14 @@ class Hoik(GameObject):
 class Flock:
     def __init__(self, boids: list[Boid]):
         self.boids = np.array(boids)
+        self.num_boids = len(boids)
 
     def avg_pos(self):
-        pass
+        p_sum = pygame.Vector2(0, 0)
+        for boid in self.boids:
+            p_sum += boid.pos
+        p_avg = p_sum / self.num_boids
+        return p_avg
 
     def avg_dir(self):
         # Returns average heading of boids in flock as a unit vector
